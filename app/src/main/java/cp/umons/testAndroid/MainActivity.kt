8b576efdd27d24fedd4e5f0a5f4c017de9ccd7dd
kazawai <bazinmaxime0@gmail.com>
2022-12-02 14:59:26 +0100

    Initial commit

---
+++ app/src/main/java/cp/umons/testAndroid/MainActivity.kt
@@ -0,0 +1,51 @@
+package cp.umons.testAndroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+
+class MainActivity : AppCompatActivity(){
+
+    lateinit var toggle : ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.nav_menu)
+        val navView : NavigationView = findViewById(R.id.navView)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.pompes_menu -> Toast.makeText(applicationContext, "Pompes clicked", Toast.LENGTH_SHORT).show()
+                R.id.boissons_menu -> Toast.makeText(applicationContext, "Boissons clicked", Toast.LENGTH_SHORT).show()
+                R.id.cocktail_menu -> Toast.makeText(applicationContext, "Cocktail clicked", Toast.LENGTH_SHORT).show()
+                R.id.next_menu -> Toast.makeText(applicationContext, "Next clicked", Toast.LENGTH_SHORT).show()
+                R.id.POWER -> Toast.makeText(applicationContext, "Power clicked", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
