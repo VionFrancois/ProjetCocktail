b50e149e77fcf7fb0fb13c588eee5bd3060510ef
kazawai <bazinmaxime0@gmail.com>
2023-02-13 17:15:41 +0100

    Bluetooth connection, permissions + socket connection

--- app/src/main/java/cp/umons/testAndroid/MainActivity.kt
+++ app/src/main/java/cp/umons/testAndroid/MainActivity.kt
@@ -3,41 +3,39 @@ package cp.umons.testAndroid
 
 import android.Manifest
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
-<<<<<<< HEAD
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-=======
-import androidx.appcompat.widget.Toolbar
-import androidx.constraintlayout.widget.ConstraintSet.Layout
->>>>>>> 62da6a6e847fe03b8bd07e3b3c9191ec3a5a2680
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import java.io.IOException
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(){
 
-    // The icon to access the menu from TODO
-    lateinit var toggle : ActionBarDrawerToggle
+    private lateinit var toggle : ActionBarDrawerToggle
 
-    private var arduinoAddress : String = ""
+    private val arduinoAddress : String = "8F5726231100"
+    private val arduinoUUID : String = "00001101-0000-1000-8000-00805F9B34FB"
+
+    private var permissionsCount = 0
 
     private var bluetoothAdapter: BluetoothAdapter? = null
     private lateinit var bluetoothManager: BluetoothManager
@@ -57,10 +55,11 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkBlePermissions()
 
         connectArduino()
 
@@ -97,6 +96,46 @@ class MainActivity : AppCompatActivity(){
         return super.onOptionsItemSelected(item)
     }
 
+
+    private fun getMissingBlePermissions(): Array<String?>? {
+        var missingPermissions: Array<String?>? = null
+        // For Android 12 and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.BLUETOOTH_SCAN)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                missingPermissions = arrayOfNulls(1)
+                missingPermissions[0] = Manifest.permission.BLUETOOTH_SCAN
+            }
+            if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.BLUETOOTH_CONNECT)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (missingPermissions == null) {
+                    missingPermissions = arrayOfNulls(1)
+                    missingPermissions[0] = Manifest.permission.BLUETOOTH_CONNECT
+                } else {
+                    missingPermissions = Arrays.copyOf(missingPermissions, missingPermissions.size + 1)
+                    missingPermissions[missingPermissions.size - 1] = Manifest.permission.BLUETOOTH_CONNECT
+                }
+            }
+        }
+        return missingPermissions
+    }
+
+    private fun checkBlePermissions() {
+        val missingPermissions = getMissingBlePermissions()
+        if (missingPermissions.isNullOrEmpty()) {
+            Log.i("TAG", "checkBlePermissions: Permissions is already granted")
+            return
+        }
+        for (perm in missingPermissions) Log.d(
+            "TAG",
+            "checkBlePermissions: missing permissions $perm"
+        )
+        permissionsCount = missingPermissions.size
+        requestPermissions(missingPermissions, 0x55)
+    }
+
     private fun enableBluetooth() : Boolean {
         this.bluetoothManager = getSystemService(BluetoothManager::class.java)
         this.bluetoothAdapter = bluetoothManager.adapter
@@ -107,7 +146,7 @@ class MainActivity : AppCompatActivity(){
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             registerForActivityResult(StartActivityForResult()){
                 if(it.resultCode != Activity.RESULT_CANCELED){
-                    // Bluetooth cancelled
+                    Toast.makeText(applicationContext, "Connection canceled", Toast.LENGTH_LONG).show()
                 }
             }.launch(enableBtIntent)
         }
@@ -123,19 +162,28 @@ class MainActivity : AppCompatActivity(){
         } else {
             this.bluetoothAdapter?.bondedDevices
         }
+
         val a : String = ""
         bDevices?.forEach { device -> a.plus(device.name) }
         Log.d("TAG", a)
         return bDevices
     }
 
-    @RequiresApi(Build.VERSION_CODES.S)
+    private fun bluetoothOff() : Boolean{
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            return false
+        }
+        this.bluetoothAdapter?.disable()
+        return true
+    }
+
     private fun connectArduino() : Boolean {
 
         // Getting the device
 
-        val bDevices = getBluetoothDevices() ?: return false
-        val bDeviceMAC = bDevices.filter{ device ->
+        var bDevices = getBluetoothDevices() ?: return false
+        var bDeviceMAC = bDevices.filter{ device ->
             device.address == this.arduinoAddress
         }
         if(bDeviceMAC.isEmpty()){
@@ -144,15 +192,27 @@ class MainActivity : AppCompatActivity(){
             if(this.arduinoDevice != null){
                 if (ActivityCompat.checkSelfPermission(
                         this, Manifest.permission.BLUETOOTH_SCAN
-                    ) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_SCAN), 0)
+                    ) == PackageManager.PERMISSION_GRANTED) {
+                    this.bluetoothAdapter?.startDiscovery()
+                    bDevices = getBluetoothDevices() ?: return false
+
+                    bDeviceMAC = bDevices.filter{ device ->
+                        device.address == this.arduinoAddress
+                    }
+
+                    if(bDeviceMAC.isEmpty()){
+                        Toast.makeText(applicationContext, "The arduino MAC address wasn't found", Toast.LENGTH_LONG).show()
+                    }
                 }
                 this.bluetoothAdapter?.cancelDiscovery()
+                return false
             }
         }
 
         // Connect to device
-
+        // If we arrive here, we know that there is only one device in bDeviceMAC, which is the one we want
+        val connectThread = ConnectThread(bDeviceMAC.first())
+        connectThread.start()
 
         return true
     }
@@ -161,6 +221,27 @@ class MainActivity : AppCompatActivity(){
         super.onDestroy()
 
         unregisterReceiver(this.receiver)
+    }
+
+    private inner class ConnectThread(device: BluetoothDevice) : Thread(){
+
+        private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            device.createRfcommSocketToServiceRecord(UUID.fromString(arduinoUUID))
+        }
+
+        override fun run() {
+            bluetoothAdapter?.cancelDiscovery()
+
+            mmSocket?.connect()
+        }
+
+        fun cancel(){
+            try{
+                mmSocket?.close()
+            }catch(e : IOException){
+                Toast.makeText(applicationContext, "Could not load the client socket " + e.message, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
